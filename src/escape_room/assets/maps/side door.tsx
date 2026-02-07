af25d432ff6843213a603966d158184695c31ac9
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-02-07 14:48:24 +0530

    path updated

--- src/escape_room/assets/maps/side door.tsx
+++ src/escape_room/assets/maps/side door.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="side door" tilewidth="16" tileheight="16" tilecount="22" columns="11">
- <image source="C:/Users/Ernest/Downloads/craftpix-net-436971-free-top-down-roguelike-game-kit-pixel-art/2 Dungeon Tileset/3 Animated objects/BigDoor_S.png" width="180" height="42"/>
+ <image source="src/escape_room/assets/images/BigDoor_S.png" width="180" height="42"/>
 </tileset>
