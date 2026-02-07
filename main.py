33b8471bc488973271128200f76cb7ce16a0d629
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-02-07 14:23:40 +0530

    test script added

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ def make_env(render_mode="human"):
     env = gym.make(
         "EscapeRoomGame-v0",
         render_mode=render_mode,
-        tmj_path="src/escape_room/assets/maps/level_one.tmj",
-        tmx_path="src/escape_room/assets/maps/level_one.tmx",
+        tmj_path="src/escape_room/assets/maps/level_two.tmj",
+        tmx_path="src/escape_room/assets/maps/level_two.tmx",
         collision_layer_name="Collision",
         doors_layer_name="Doors",
         door_tiles_layer_name="DoorsTiles",
         rooms_layer_name="Rooms",
-        time_limit_steps=500,
+        time_limit_steps=3000,
         # optional goal args (only if your env supports them)
         goal_layer_name="Goal_Room",
         goal_reward=10.0,
@@ -61,11 +61,20 @@ def test_scripted(render_mode="human"):
 
     # actions: 0 noop, 1 up, 2 down, 3 left, 4 right, 5 interact
     scripted_actions = (
-        [1] * 60 +   # UP
-        [5] * 5  +   # INTERACT
+        [1] * 12 +   # UP
         [3] * 50 +   # LEFT
-        [5] * 5  +   # INTERACT
+        [5] * 15 +   # INTERACT
+        [3] * 40 +    # LEFT
+        [0] * 50 +
+        [4] * 60 +   # right
+        [5] * 15 +    # INTERACT
+        [4] * 160 +    # RIGHT
+        [0] * 50 +
+        [3] * 80 +   # LEFT
+        [5] * 15 +   # INTERACT
         [3] * 40     # LEFT
+
+        
     )
 
     score = 0.0
@@ -75,10 +84,10 @@ def test_scripted(render_mode="human"):
         done = terminated or truncated
         score += float(reward)
 
-        print(
-            f"step={i:03d} action={action} reward={reward:.2f} "
-            f"terminated={terminated} truncated={truncated} room={info.get('current_room')}"
-        )
+        # print(
+        #     f"step={i:03d} action={action} reward={reward:.2f} "
+        #     f"terminated={terminated} truncated={truncated} room={info.get('current_room')}"
+        # )
 
         if done:
             print("Episode ended early (likely goal reached).")
@@ -89,6 +98,42 @@ def test_scripted(render_mode="human"):
 
     print("Final score:", score)
     env.close()
+
+
+def test_scripted_hardcoded(render_mode="human"):
+    env = make_env(render_mode=render_mode)
+    obs, info = env.reset()
+
+    scripted_actions = (
+        # go to room 1
+        
+        #[2]*70 + [3]*20 + [2]*15 + [5]*15 + [2]*20 + [5]*15 + [2]*20 
+         [1]*12 + [4]*65 + [5]*15 + [4]*35 + [2]*10 + [6]*1 + [1]*10 + [3]*12 + [5]*15 + [3]*40 +
+         [1]*40 + [4]*65 + [5]*15 + [4]*50 + [6]*1 + [3]*20 + [5]*15 + [3]*42 +  
+         [2]*155 + [5]*15 + [2]*30 + [4]*10 + [6]*1 + [3]*10 + [1]*30 +
+         [3]*29 + [2]*35 + [5]*15 + [2]*20 + [3]*20 + [6]*1 + [4]*24 + [1]*20 + [5]*15 + [1]*30 +
+         [1]*60 + [3]*20 + [5]*15 + [3]*40 
+        #[3]*20 + [2]*80 + [5]*15 + [2]*10
+    )
+
+    score = 0.0
+    for i, action in enumerate(scripted_actions):
+        obs, reward, terminated, truncated, info = env.step(action)
+        score += reward
+
+        if reward != 0:
+            print(
+                f"step={i} reward={reward:.2f} "
+                f"levers_on={info['lever_on']} unlocked={info['goal_unlocked']}"
+            )
+
+        if terminated or truncated:
+            print("Goal reached / episode ended")
+            break
+
+    print("Final score:", score)
+    env.close()
+
 
 
 def agent_training(render_mode=None):
@@ -112,7 +157,7 @@ def agent_training(render_mode=None):
 
 def eval():
     config = {
-        "render_mode": "human",     # None for faster training, "human" to watch
+        "render_mode": None,     # None for faster training, "human" to watch
         "time_limit_steps": 500,
         "gamma": 0.99,
         "eps_clip": 0.2,
@@ -125,12 +170,17 @@ def eval():
         "log_freq": 1000 * 2,
         "print_freq": 2000 * 5,
         "save_model_freq": int(1e5),
-        "state_dim": 31,
-        "action_dim": 5,
     }
 
+    env = make_env(render_mode=None)
+    config['state_dim'] = env.observation_space.shape[0]
+    config['action_dim'] = env.action_space.n
+    print("obs shape:", env.observation_space.shape[0])
+    print("action dim:", env.action_space.n)
     agent = PPO(state_dim=config['state_dim'], action_dim=config['action_dim'], config=config)
-    evaluate_agent_and_save_gifs(agent=agent, make_env=make_env, num_episodes=2, out_dir="eval_gifs", gif_prefix="eval_ep", fps=30)
+    agent.load_safetensors(checkpoint_path="runs\\EscapeRoom\\run_000\\models")
+    print("Loaded agent from checkpoint.")
+    evaluate_agent_and_save_gifs(agent=agent, make_env=make_env, num_episodes=2, out_dir="eval_gifs", gif_prefix="eval_ep", fps=30, mode="rgb_array")
 
 
 
@@ -139,7 +189,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "mode",
-        choices=["test-random", "test-scripted", "agent-training", "eval"],
+        choices=["test-random", "test-scripted", "agent-training", "eval", "test2"],
         help="Which script to run",
     )
     parser.add_argument(
@@ -159,6 +209,8 @@ def main():
         agent_training(render_mode=render_mode)
     elif args.mode == "eval":
         eval()
+    elif args.mode == "test2":
+        test_scripted_hardcoded(render_mode="human" if args.human else "human")
 
 
 if __name__ == "__main__":
